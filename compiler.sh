81e5704c788b239a70b733b62be025cb19ab6f7e
DyhaCreator <andropet228@gmail.com>
2023-07-05 15:06:09 -0400

    Test SFML

--- compiler.sh
+++ compiler.sh
@@ -1,5 +1,6 @@
-echo compile
-g++ main.cpp *.h
-rm *.h.gch
+echo "compile"
+g++ -c main.cpp -I /SFML/include
+g++ main.o -o sfml-app -L /SFML/lib -lsfml-graphics -lsfml-window -lsfml-system
+rm main.o
 echo "start>"
-./a.out
+./sfml-app
